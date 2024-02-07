19d335d4d233d94599bb86dbc66cdcf63c8cfe69
mhabibinfo <muhammadhabib123@gmail.com>
2024-02-07 08:31:21 +0500

    Update 3 a category model

--- espProject/prod.py
+++ espProject/prod.py
@@ -6,7 +6,8 @@ from .settings import BASE_DIR
 SECRET_KEY = os.environ['SECRET']
 #ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 ALLOWED_HOSTS = ['employeeportal1a.azurewebsites.net']
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+#CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://employeeportal1a.azurewebsites.net']
 DEBUG = False
 
 # WhiteNoise configuration
